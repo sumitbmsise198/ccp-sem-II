2cc0f91ccc6ab068593eff6dcd26389ca85a6c2e
sumitbmsise198 <83854453+sumitbmsise198@users.noreply.github.com>
2021-06-14 09:48:29 +0530

    Create 14.june.21 C program to print even numbers from M to N.c

---
+++ 14.june.21 C program to print even numbers from M to N.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void print(int m, int n);
+int main() {
+    int m,n;
+    printf("enter the value of m and n:\n");
+    printf("m = ");
+    scanf("%d", &m);
+    printf("n = ");
+    scanf("%d", &n);
+    if(m>=n){
+        printf("NOT POSSIBLE for the given numbers");
+    }
+    else{
+    print(m,n);}
+    return 0;
+}
+void print(int m, int n){
+    do{
+        if(m%2==0){
+            printf("%d \n", m);
+            m=m+1;
+        }
+        else{
+        m=m+1;}
+    }while(m<=n);
+    return 0;
+}
