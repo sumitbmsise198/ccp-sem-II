012a59715caa07e4fd459fae5388d71595a4011a
sumitbmsise198 <83854453+sumitbmsise198@users.noreply.github.com>
2021-07-05 10:47:45 +0530

    Create 5.july.2021  Develop a C program to copy one string to another string and find its length without using built in functions.c

---
+++ 5.july.2021  Develop a C program to copy one string to another string and find its length without using built in functions.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char str[100],strr[50];
+    int i;
+    printf("Enter string str1\n");
+    scanf("%s",str);
+        printf("Enter string str1\n");
+    scanf("%s",strr);
+    for(i=0;str[i]!='\0';i++)
+    {
+        strr[i] = str[i];
+    }
+    strr[i]='\0';
+    printf("Copied String(str2) is %s and its length is %d",strr,i);
+    return 0;
+}
