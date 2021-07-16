40686d366667d9982db825fc091c54f7e29acd4a
sumitbmsise198 <83854453+sumitbmsise198@users.noreply.github.com>
2021-07-16 09:52:34 +0530

    Update 5.july.2021 Develop a C program to create student structure, read two student details and print the student details  who has scored the highest.c

--- 5.july.2021 Develop a C program to create student structure, read two student details and print the student details  who has scored the highest.c
+++ 5.july.2021 Develop a C program to create student structure, read two student details and print the student details  who has scored the highest.c
@@ -1,20 +1,21 @@
-#include <stdio.h>  
-struct student{
-  int rollno;
-  char name[20];
-  char sec[2];
-  char dep[4];
-  int fees;
-  int result;
+#include <stdio.h>
+struct student
+{
+    int roll;
+    char name[20];
+    char section[20];
+    int result;
+    int fees;
+    char dep[20];
 };
 struct student getinfo();
 void print(struct student s1);
-int main() {
-   struct student s1,s2;
-  printf("Enter details of 1st Student\n");
-  s1 = getinfo();
-  printf("Enter details of 2nd Student\n");
-  s2 = getinfo();
+int main(){
+    struct student s1,s2;
+    printf("Enter details of first student");
+    s1 = getinfo();
+    printf("Enter details of second student");
+    s2 = getinfo();
     if(s1.result>s2.result){
         print(s1);
     }
@@ -25,26 +26,26 @@ int main() {
 }
 struct student getinfo(){
     struct student s1;
-    printf("roll No. ");
-  scanf("%d",&s1.rollno);
-  printf("Name: ");
-  scanf("%s",s1.name);
-  printf("Section: ");
-  scanf("%s",s1.sec);
-  printf("Fees: ");
-  scanf("%d",&s1.fees);
-  printf("Result: ");
-  scanf("%d",&s1.result);
-    printf("Department: ");
-  scanf("%s",s1.dep);
-  return s1;
+    printf("\nRoll No: - \n");
+    scanf("%d",&s1.roll);
+    printf("Name: -\n");
+    scanf("%s",s1.name);
+    printf("Section: -\n");
+    scanf("%s",s1.section);
+    printf("Result:- \n");
+    scanf("%d",&s1.result);
+    printf("Fees: - \n");
+    scanf("%d",&s1.fees);
+    printf("Department:-\n");
+    scanf("%s",s1.dep);
+    printf("\n");
+    return s1;
 }
 void print(struct student s1){
-    printf("The details of student who got highest marks are as follows");
-    printf("Roll No.: %d\n",s1.rollno);
-    printf("Name: %s\n",s1.name);
-    printf("Section: %s\n",s1.sec);
-    printf("Department: %s\n",s1.dep);
-    printf("Fees: %d\n",s1.fees);
-    printf("Result = %d",s1.result);
+    printf("Roll No: - %d \n",s1.roll);
+    printf("Name: -%s\n",s1.name);	
+    printf("Section: %s-\n",s1.section);
+    printf("Result:- %d\n",&s1.result);
+    printf("Fees: - %d\n",&s1.fees);
+    printf("Department:- %s\n",s1.dep);
 }
